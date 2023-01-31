a1a0b1199ed4fa9f04150d43ebd913e893ba79c9
adriano <53664407+adriano-r@users.noreply.github.com>
2023-01-31 01:39:49 -0300

    Add files via upload
    - Buscando registros do banco com Spring Data

--- regescweb/src/main/java/com/springMVC/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/com/springMVC/regescweb/controllers/ProfessorController.java
@@ -1,32 +1,31 @@
 package com.springMVC.regescweb.controllers;
 
 import com.springMVC.regescweb.models.Professor;
-import com.springMVC.regescweb.models.StatusProfessor;
-import org.springframework.boot.Banner;
+import com.springMVC.regescweb.repositories.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.lang.reflect.Array;
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class ProfessorController {
+    @Autowired
+    private ProfessorRepository professorRepository;
+
     @GetMapping("/professores")
     public ModelAndView index(){
-        Professor batman = new Professor("Batman", new BigDecimal(5000.0), StatusProfessor.ATIVO);
-        batman.setId(1L);
-        Professor coringa = new Professor("Coringa", new BigDecimal(1000.0), StatusProfessor.APOSENTADO);
-        coringa.setId(2L);
-        Professor chaves = new Professor("Chaves", new BigDecimal(8000.0), StatusProfessor.INATIVO);
-        chaves.setId(3L);
-        List<Professor> professors = Arrays.asList(batman, coringa, chaves);
+        List<Professor> professores = this.professorRepository.findAll();
 
         ModelAndView mv = new ModelAndView("professores/index");
-        mv.addObject("professores", professors);
+        mv.addObject("professores", professores);
 
         return mv;
+    }
+
+    @GetMapping("/professor/new")
+    public String nnew(){
+        return "professores/new";
     }
 }
